baf3d7f3190d1804b194f2784f2305b0e47f33c9
karenhernandez <khernandez0329@gmail.com>
2023-11-08 10:31:43 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 // your code here
+
+const cakesAPI = "http://localhost:3000/cakes";
+
+const cakeName = document.getElementById('cake-name')
+let cakeDetails = document.getElementById('cake-details')
+
+
+
+fetch(cakesAPI)
+.then(res => res.json())
+.then(cakes => {
+    cakeDetails = cakes;
+    renderCakes(cakeDetails)
+})
+
+
+
+
+function renderCakes(dessert){
+    const cakeReview = document.createElement('li');
+    cakeReview.classList.add('cakes');
+    cakeDetails.append(cakeReview);
+
+    cakeReview.innerHTML =`
+    <h2> ${dessert.name}</h2>
+    <img src="${dessert.image_URL}" />
+    <p> ${description}</p>
+
+    `;
+    
+}
+
+function renderCakes(cakes){
+    cakeReview.innerHTML = '';
+    cakes.forEach(renderCakes);
+  }
